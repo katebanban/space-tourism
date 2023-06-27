c90d8611e663a1bcf56d9a1afa17262e71d4e7e2
katebanban <101980471+katebanban@users.noreply.github.com>
2023-06-27 13:48:18 +0300

    finished adapting header

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ $(function(){
 		nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow right">',
 	});
 });
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', function () {
+	menuList.classList.toggle('menu__list--active');
+	menuBtn.classList.toggle('menu__btn--active');
+	document.querySelector('body').classList.toggle('no-scroll');
+});
