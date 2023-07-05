820ac748994ddc00f705e1dba44a01320c4ca89b
katebanban <101980471+katebanban@users.noreply.github.com>
2023-07-05 16:36:49 +0300

    finished adapting the "how-to" section
    version 2

--- js/main.js
+++ js/main.js
@@ -13,3 +13,41 @@ menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('menu__btn--active');
 	document.querySelector('body').classList.toggle('no-scroll');
 });
+
+let foreighObject = document.querySelectorAll('foreignObject');
+
+let x1 = foreighObject[0].getAttribute('x');
+let y1 = foreighObject[0].getAttribute('y');
+
+let x2 = foreighObject[1].getAttribute('x');
+let y2 = foreighObject[1].getAttribute('y');
+
+let x3 = foreighObject[2].getAttribute('x');
+let y3 = foreighObject[2].getAttribute('y');
+
+function myFunction(screen) {
+	if (screen.matches) { // Если медиа запрос совпадает
+		foreighObject[0].setAttribute('x', '15');
+		foreighObject[0].setAttribute('y', '398');
+		
+		foreighObject[1].setAttribute('x', '130');
+		foreighObject[1].setAttribute('y', '520');
+		
+		foreighObject[2].setAttribute('x', '570');
+		foreighObject[2].setAttribute('y', '420');
+	} else {
+		foreighObject[0].setAttribute('x', x1);
+		foreighObject[0].setAttribute('y', y1);
+		
+		foreighObject[1].setAttribute('x', x2);
+		foreighObject[1].setAttribute('y', y2);
+		
+		foreighObject[2].setAttribute('x', x3);
+		foreighObject[2].setAttribute('y', y3);
+	}
+}
+
+let myMediaWidth = window.matchMedia("(max-width: 425px)")
+myFunction(myMediaWidth) // Вызов функции прослушивателя во время выполнения
+myMediaWidth.addListener(myFunction) // Присоединить функцию прослушивателя при изменении состояния
+
